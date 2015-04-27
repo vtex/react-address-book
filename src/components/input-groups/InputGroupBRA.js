a611fe7b6e409ed481a34c7fe85248c8b90b348c
Guilherme Rodrigues <gadr90@gmail.com>
2015-04-27 19:58:52 -0300

    Add fields to BRA form

--- src/components/input-groups/InputGroupBRA.js
+++ src/components/input-groups/InputGroupBRA.js
@@ -7,13 +7,47 @@ export default function (address) {
   return (
     <div className={className}>
       <div className='row'>
-        <CEP className='col-sm-6' value={address.postalCode}/>
+        <CEP className='col-sm-4' value={address.postalCode}/>
       </div>
       <div className='row'>
-        <Text className='col-sm-6'
+        <Text className='col-sm-8'
           name='street'
           required
           value={address.street}/>
+      </div>
+      <div className='row'>
+        <Text className='col-sm-4'
+          name='number'
+          required
+          value={address.number}/>
+        <Text className='col-sm-4'
+          name='complement'
+          required
+          value={address.complement}/>
+        <Text className='col-sm-4'
+          name='neighborhood'
+          required
+          value={address.neighborhood}/>
+      </div>
+      <div className='row'>
+        <Text className='col-sm-4'
+          name='city'
+          required
+          value={address.city}/>
+        <Text className='col-sm-4'
+          name='state'
+          required
+          value={address.state}/>
+        <Text className='col-sm-4'
+          name='country'
+          required
+          value={address.country}/>
+      </div>
+      <div className='row'>
+        <Text className='col-sm-4'
+          name='receiverName'
+          required
+          value={address.receiverName}/>
       </div>
     </div>
   );
--- src/i18n/pt.js
+++ src/i18n/pt.js
@@ -3,6 +3,17 @@ export default {
   "edit": "Editar",
   "save": "Salvar",
   "street": "Rua",
+  "number": "Número",
+  "complement": "Complemento",
+  "neighborhood": "Bairro",
+  "city": "Cidade",
+  "state": "Estado",
+  "country": "País",
+  "receiverName": "Destinatário",
+  "countries": {
+    "BRA": "Brasil",
+    "ARG": "Argentina"
+  },
   "validation": {
     "required": "Este campo é obrigatório",
     "alphaNumericPunctuation": "Por favor, digite apenas letras e números",
