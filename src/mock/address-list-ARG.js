ee14e60534719f7da2476f2be506872d5029ffc3
Guilherme Rodrigues <gadr90@gmail.com>
2015-04-29 18:03:08 -0300

    Add address list for ARG

---
+++ src/mock/address-list-ARG.js
@@ -0,0 +1,33 @@
+export default [
+  {
+    "addressType": "residential",
+    "receiverName": "José",
+    "addressId": "-1429301413320",
+    "postalCode": "1414",
+    "city": "Buenos Aires",
+    "state": "Buenos Aires",
+    "country": "ARG",
+    "street": "Av Córdoba",
+    "number": "1438",
+    "neighborhood": "Recoleta",
+    "complement": "",
+    "reference": null,
+    "geoCoordinates": [],
+    "selected": true
+  },
+  {
+    "addressType": "residential",
+    "receiverName": "Jorge",
+    "addressId": "-1429301413320",
+    "postalCode": "1414",
+    "city": "Buenos Aires",
+    "state": "Buenos Aires",
+    "country": "ARG",
+    "street": "Florida",
+    "number": "343",
+    "neighborhood": "Retiro",
+    "complement": "",
+    "reference": null,
+    "geoCoordinates": []
+  }
+];
